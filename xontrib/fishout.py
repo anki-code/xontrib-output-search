477164124e25c1e28947fea1ffaa56c1620250c3
anki-code <anki-code>
2020-04-23 02:41:52 +0300

    0.0.3

--- xontrib/fishout.py
+++ xontrib/fishout.py
@@ -5,16 +5,33 @@ import re
 _symbol = str(__xonsh__.env.get('XONTRIB_FISHOUT_SYMBOL') or 'o')
 _functions = dict(__xonsh__.env.get('XONTRIB_FISHOUT_FUNCTIONS') or {'in': '-', 'startswith': '='})
 
+clean_regexp = re.compile(r'[\n\r\t]')
 color_regexp = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+framed_regexp = re.compile(r'^["\'{,:](.+?)[,}"\':]+$')
+
+def _generator(token):
+    """
+    Create alternatives for token. Example: token '"Value"' has unquoted 'Value' alternative.
+    """
+    token_variation = []
+    if len(token) > 2:
+        g = framed_regexp.match(token)
+        if g:
+            token_variation += [g.group(1)]
+    return token_variation
+
 def _tokenizer(text, prefix='', func='in'):
-    tokens = color_regexp.sub('', str(text)).strip().replace('\n', ' ').replace('\r', ' ').split(' ')
+    """
+    Split text to tokens.
+    """
+    tokens = clean_regexp.sub(' ', color_regexp.sub('', str(text))).strip().split(' ')
     selected_tokens = []
     for t in tokens:
         if len(t) > 1:
             if func == 'in' and prefix[2:].lower() in t.lower():
-                selected_tokens.append(t)
+                selected_tokens += [t] + _generator(t)
             elif func == 'startswith' and t.lower().startswith(prefix[2:].lower()):
-                selected_tokens.append(t)
+                selected_tokens += [t]
     return selected_tokens
 
 def _xontrib_fishout_completer(prefix, line, begidx, endidx, ctx):
