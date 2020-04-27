173236ab8cbc9590393c6db6fb41775112960916
anki-code <anki-code>
2020-04-27 17:05:15 +0300

    Fix windows install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 try:
-    with open('README.md') as fh:
+    with open('README.md', 'r', encoding='utf-8') as fh:
         long_description = fh.read()
 except (IOError, OSError):
     long_description = ''
