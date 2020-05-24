b0af5e23bc021ac45c53b4dc487bfa89420ad2a1
a <a>
2020-05-24 15:23:45 +0300

    0.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setup(
     name='xontrib-output-search',
-    version='0.5.0',
+    version='0.5.1',
     license='BSD',
     author='anki-code',
     author_email='author@example.com',
